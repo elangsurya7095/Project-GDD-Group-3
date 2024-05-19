6f0b3e452fefd32fb03a02df9353f1d2077385eb
Farchan Mungis Yudistira <farhan.mungis@gmail.com>
2024-05-20 01:15:17 +0700

    main menu
    fungsi button , pop up ( ok )

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject menupanel1;
+    public GameObject menupanel2;
+    // public GameObject setting;
+    // Start is called before the first frame update
+    void Start()
+    {
+        menupanel1.SetActive(true);
+        menupanel2.SetActive(false);
+        // setting.SetActive(false);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartButtonPanel2(string scenename){
+        SceneManager.LoadScene(scenename);
+        }
+
+    public void StartButtonPanel1(){
+        menupanel1.SetActive(false);
+        menupanel2.SetActive(true); 
+    }
+
+    // public void SettingButton(){
+    //     menupanel1.SetActive(false);
+    //     menupanel2.SetActive(false);
+    //     setting.SetActive(true);
+    // }
+
+    // public void BackButton(){
+    //     menupanel1.SetActive(false);
+    //     menupanel2.SetActive(true);
+    //     setting.SetActive(false);
+    // }
+    
+}
